4b1ec0ad9665b1e1c4c44ee90ca7c9c3e2960f37
kenigbolo <expensivestephen@hotmail.com>
2020-07-21 12:53:12 +0300

    Add carousel forward arrow trigger transition

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 
 window.addEventListener('DOMContentLoaded', (event) => {
-  const switchmenu = document.querySelector(".switch-menu");
-  const mobileMenu = document.querySelector(".mobile-menu");
+  const switchmenu = document.querySelector('.switch-menu');
+  const mobileMenu = document.querySelector('.mobile-menu');
   const carouselTriggers = document.querySelectorAll('.carousel-trigger');
   const carouselItems = document.querySelectorAll('.carousel-item');
   const carouselArrowButton = document.getElementById('js-next-carousel');
-  let carouselId = '';
+  let carouselId = 'carousel-1';
+  const carouselIdList = ['carousel-1', 'carousel-2', 'carousel-3', 'carousel-4'];
+  const lastIndex = 3; // The JavaScript biggest index of the carouselIdList
 
   switchmenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
-  })
+  });
 
   const removeAllActiveStates = (elementsList) => {
     elementsList.forEach((element) => element.classList.remove('active'));
@@ -19,18 +21,24 @@ window.addEventListener('DOMContentLoaded', (event) => {
     carouselItems.forEach((item) => item.classList.add('carousel-transition'));
   }
 
+  const toggleTrigger = (target, arrowToggle = false) => {
+    removeAllActiveStates(carouselTriggers);
+    target.classList.add('active');
+    carouselId = arrowToggle ? target.id : target.id.substring(8);
+    setAllCarouselItemsToHidden();
+    document.getElementById(carouselId).classList.remove('carousel-transition');
+    document.getElementById(carouselId).classList.add('active');
+  }
+
   carouselTriggers.forEach((carouselElement) => {
-    carouselElement.addEventListener('click', ({ target }) => {
-      removeAllActiveStates(carouselTriggers);
-      target.classList.add('active');
-      carouselId = target.id.substring(8);
-      setAllCarouselItemsToHidden()
-      document.getElementById(carouselId).classList.remove('carousel-transition');
-      document.getElementById(carouselId).classList.add('active');
-    });
+    carouselElement.addEventListener('click', ({ target }) => toggleTrigger(target));
   });
 
-  carouselArrowButton.addEventListener('click', ({ target }) => {
-    console.log(carouselId);
+  carouselArrowButton.addEventListener('click', () => {
+    const currentIndex = carouselIdList.findIndex(id => id === carouselId);
+    const target = currentIndex === lastIndex
+      ? document.getElementById(carouselIdList[0])
+      : document.getElementById(carouselIdList[currentIndex + 1]);
+    toggleTrigger(target, true);
   });
 });
