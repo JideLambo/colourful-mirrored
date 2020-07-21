b8056f8245ed71452fad7806b332e3a725c39c6d
kenigbolo <expensivestephen@hotmail.com>
2020-07-21 12:18:41 +0300

    🎨wip - Carousel Forward Arrow

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
   const mobileMenu = document.querySelector(".mobile-menu");
   const carouselTriggers = document.querySelectorAll('.carousel-trigger');
   const carouselItems = document.querySelectorAll('.carousel-item');
+  const carouselArrowButton = document.getElementById('js-next-carousel');
+  let carouselId = '';
 
   switchmenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
@@ -21,10 +23,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
     carouselElement.addEventListener('click', ({ target }) => {
       removeAllActiveStates(carouselTriggers);
       target.classList.add('active');
-      const carouselId = target.id.substring(8);
+      carouselId = target.id.substring(8);
       setAllCarouselItemsToHidden()
       document.getElementById(carouselId).classList.remove('hidden');
       document.getElementById(carouselId).classList.add('active');
     });
+  });
+
+  carouselArrowButton.addEventListener('click', ({ target }) => {
+    console.log(carouselId);
   });
 });
