e48857c0e0b2b96edc71c9754a69767636fc5db2
kenigbolo <expensivestephen@hotmail.com>
2020-08-08 19:33:39 +0300

    🎨Simulate toggle changes inside the news content toggling

--- index.js
+++ index.js
@@ -101,14 +101,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Event listener for tab-pills-1 active state toggling
   tabPillsTwoItems.forEach((tabPill) => {
-    tabPill.addEventListener('click', () => {
+    tabPill.addEventListener('click', ({ target }) => {
       tabPillsTwoItems.forEach((element) => {
         element.classList.add('bg-color-white2', 'text-black')
         element.classList.remove('bg-black', 'text-white')
       });
-      tabPill.classList.remove('bg-color-white2', 'text-black')
+      tabPill.classList.remove('bg-color-white2', 'text-black',)
       tabPill.classList.add('bg-black', 'text-white');
       // Switch the content on display
+      document.querySelectorAll('.news-content').forEach((element) => {
+        element.classList.add('hidden');
+      });
+      document.getElementById(`${target.id}-content`).classList.remove('hidden');
     });
   });
 });
