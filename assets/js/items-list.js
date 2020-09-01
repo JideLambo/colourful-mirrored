490282ae08be88d1c4f158232509abb82128b645
kenigbolo <expensivestephen@hotmail.com>
2020-09-01 21:18:20 +0300

    Add video toggling functionality, minor updates

--- assets/js/items-list.js
+++ assets/js/items-list.js
@@ -2,11 +2,21 @@ window.addEventListener('DOMContentLoaded', () => {
   let isNewestDropdownOpen = false;
   const dropdownTrigger = document.getElementById('newest-item-dropdown-trigger');
   const dropdownMenu = document.getElementById('newest-item-dropdown-menu');
+  const videoPlayerDiv = document.getElementById('video-player');
+  const videoTrigger = document.querySelectorAll('.video-trigger');
+  const isPlayerOpen = false;
+  
 
   dropdownTrigger.addEventListener('click', () => {
     isNewestDropdownOpen = !isNewestDropdownOpen;
     isNewestDropdownOpen
       ? dropdownMenu.classList.remove('hidden')
       : dropdownMenu.classList.add('hidden');
+  })
+
+  videoTrigger.forEach((trigger) => {
+    trigger.addEventListener('click', () => {
+      videoPlayerDiv.classList.remove('hidden');
+    })
   })
 })
