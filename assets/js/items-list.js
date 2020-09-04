34c0a92246ab7fd63eb8a9379880c31587a32c19
Jide Lambo <jide.lambo@nordcloud.com>
2020-09-04 18:31:02 +0300

    Add close button on video window

--- assets/js/items-list.js
+++ assets/js/items-list.js
@@ -1,24 +1,32 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   let isNewestDropdownOpen = false;
-  const dropdownTrigger = document.getElementById('newest-item-dropdown-trigger');
-  const dropdownMenu = document.getElementById('newest-item-dropdown-menu');
-  const videoPlayerWrapper = document.getElementById('video-player-wrapper');
-  const videoTrigger = document.querySelectorAll('.video-trigger');
-  const videoPlayer = document.getElementById('video-player');
-  
+  const dropdownTrigger = document.getElementById(
+    "newest-item-dropdown-trigger"
+  );
+  const dropdownMenu = document.getElementById("newest-item-dropdown-menu");
+  const videoPlayerWrapper = document.getElementById("video-player-wrapper");
+  const closeButton = document.getElementById("close-video");
+  const videoTrigger = document.querySelectorAll(".video-trigger");
+  const videoPlayer = document.getElementById("video-player");
 
-  dropdownTrigger.addEventListener('click', () => {
+  dropdownTrigger.addEventListener("click", () => {
     isNewestDropdownOpen = !isNewestDropdownOpen;
     isNewestDropdownOpen
-      ? dropdownMenu.classList.remove('hidden')
-      : dropdownMenu.classList.add('hidden');
-  })
+      ? dropdownMenu.classList.remove("hidden")
+      : dropdownMenu.classList.add("hidden");
+  });
+
+  closeButton.addEventListener("click", () => {
+    // Set the new url for the videoPlayer. For example
+    // videoPlayer.src = "https://www.youtube.com/watch?v=39iyB787lsw"
+    videoPlayerWrapper.classList.add("hidden");
+  });
 
   videoTrigger.forEach((trigger) => {
-    trigger.addEventListener('click', () => {
+    trigger.addEventListener("click", () => {
       // Set the new url for the videoPlayer. For example
       // videoPlayer.src = "https://www.youtube.com/watch?v=39iyB787lsw"
-      videoPlayerWrapper.classList.remove('hidden');
-    })
-  })
-})
+      videoPlayerWrapper.classList.remove("hidden");
+    });
+  });
+});
